d07e7a8a25841db0e304a22ff9d2cd575b700f65
ArsalanWahidAsghar <arsalanwahid1993@gmail.com>
2019-11-19 18:43:10 +0500

    solved git bug

--- wiki-plant-data/Callicarpa_americana.py
+++
@@ -1,8 +0,0 @@
-Callicarpa_americana_text = '''
-Callicarpa americana, the American beautyberry, is an open-habit, native shrub of the Southern United States which is often grown as an ornamental in gardens and yards. American beautyberries produce large clusters of purple berries, which birds and deer eat, thus distributing the seeds.
-The raw berries, while palatably sweet, are suitable for human consumption only in small amounts, because they are astringent. Some people have reported mild stomach cramps after consumption. The berries are also used in jellies and wine. The roots are used to make herbal tea. As a folk remedy it has been claimed that "fresh, crushed leaves of American beautyberry, Callicarpa americana ... helped keep biting insects away from animals such as horses and mules".[1] A chemical compound isolated from the plant, callicarpenal, was effective as a mosquito repellent in a laboratory experiment using a simulated skin model.[2]
-
-The berries ripen in September through October and are a favorite among wild bird species including cardinals, mockingbirds, finches, woodpeckers and more. Beautyberry is commonly planted in landscape designs to attract wildlife because of the food source the berries provide and the cover animals get from the shrub itself.
-The native range of C. americana extends from Maryland to Florida, west to Texas and Arkansas, and also Mexico, Bermuda, the Bahamas and Cuba.
-Plants with white berries are found in cultivation under the name Callicarpa americana var. lactea;[4][5][6] not all authorities recognize this as a distinct variety (in the sense of the botanical rank below subspecies)
-'''
