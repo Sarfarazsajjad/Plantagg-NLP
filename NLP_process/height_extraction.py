9f1401db11d082503a5c9e4c14a82e249967e63c
ArsalanWahidAsghar <arsalanwahid1993@gmail.com>
2019-11-20 21:30:39 +0500

    plant heights stored in csv file

--- NLP_process/height_extraction.py
+++ NLP_process/height_extraction.py
@@ -3,7 +3,8 @@ import wikipedia as wikipedia
 import nltk as nltk
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
-
+import csv
+import os
 
 def getPage(plantName):
     return wikipedia.page(plantName).content  # get page data in string;
@@ -19,8 +20,7 @@ def sentenceTokenize(sentencesArray):
 
 def lemmitizedList(sents):
     lmtzr = WordNetLemmatizer()
-    lemmatizedSentences = [[lmtzr.lemmatize(word) for word in nltk.word_tokenize(s)]
-                           for s in sents]
+    lemmatizedSentences = [[lmtzr.lemmatize(word) for word in nltk.word_tokenize(s)] for s in sents]
     return lemmatizedSentences
 
 
@@ -29,11 +29,20 @@ def findHeightWordInSentences(lemmatizedSentences,plantName):
         for word in sentence:
             if (word == 'height' or word=='tall'):
                 sentenceThatContainHeightKeyword = " ".join(str(x) for x in sentence)
-                print("plant Name is :["+plantName+"]"+" ["+sentenceThatContainHeightKeyword+"]");
+                print("plant Name is :" ,plantName, sentenceThatContainHeightKeyword);
+                with open('plantData.csv','a',newline='') as csvfile:
+                    writer = csv.writer(csvfile)
+                    writer.writerow([plantName,sentenceThatContainHeightKeyword,""])
 
+def clearFile():
+    with open('plantData.csv','w+',newline='') as csvfile:
+        pass
 
 if __name__ == '__main__':
     plantNames = ['Acer_palmatum', 'Anisacanthus_quadrifidus','buchloe_dactyloides','Callicarpa_americana','Cercis_canadensis','Chrysactinia_mexicana','Coreopsis_lanceolata','Cotinus_coggygria','Echinacea_paradoxa','Euphorbia_myrsinites','Hesperaloe_funifera','Hydrangea_quercifolia','juniperus_horizontalis','lantana_urticoides','Liriope_muscari','magnolia_soulangeana','Miscanthus_sinensis','Muhlenbergia_lindheimeri','Nerium','Opuntia_rufida','Phlox_divaricata','Phoenix_reclinata','Pyrus_calleryana','Rhododendron_japonicum','Rubus_idaeus','Salvia_guaranitica','Salvia_pachyphylla']
+    singlePlant = ['Acer_palmatum','Anisacanthus_quadrifidus','buchloe_dactyloides']
+    clearFile()
+    
     for plantName in plantNames:
         page = getPage(plantName);
 
