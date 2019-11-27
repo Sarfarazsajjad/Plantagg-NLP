fe1d96fc035fbb4c9da1f478fa52c510657801b3
ArsalanWahidAsghar <arsalanwahid1993@gmail.com>
2019-11-27 23:38:51 +0500

    plant data update

--- NLP_process/saveWikiPages.py
+++ NLP_process/saveWikiPages.py
@@ -1,11 +1,11 @@
 import wikipedia as wikipedia
 import csv
 
-def savePages(srNo,pageName,wikiLink):
+def savePages(srNo,pageName,category,wikiLink):
   pageContent = wikipedia.page(pageName).content
   with open('plantData.csv','a',newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow([srNo,pageName,wikiLink,pageContent,""])
+    writer.writerow([srNo,pageName,category,wikiLink,pageContent,""])
     print("saved",pageName)
 
 def loadPlantNames():
@@ -13,7 +13,7 @@ def loadPlantNames():
   # Clear the plant data File to prevent data override 
   with open('plantData.csv','w+',newline='') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(['SR No','Plant Name','Wiki link','Page Data',""])
+    writer.writerow(['SR No','PlantName','Category','Wiki link','Page Data',""])
 
   with open('plantList.csv') as csvfile:
     next(csvfile)
@@ -21,30 +21,13 @@ def loadPlantNames():
     for row in reader:
       pageCounter += 1
       pageName = row[1]
-      wikiLink = row[2]
+      category = row[2]
+      wikiLink = row[3]
       if(pageName == 'NILL'):
         pass
       else:
-        savePages(pageCounter,pageName,wikiLink)
+        savePages(pageCounter,pageName,category,wikiLink)
 
 
 if __name__ == '__main__':
-  # plantNames = [
-  #   'Linnaea_×_grandiflora','Acer_palmatum','Acer_truncatum','Aesculus_pavia',
-  #   'Agave_parryi','Aloe_vera','Anisacanthus_quadrifidus',
-  #   'Asparagus_Densiflorus','Astrolepis_sinuata',
-  #   'Bignonia_capreolata','buchloe_dactyloides','Buddleja_davidii','Buxus_microphylla',
-  #   'Buxus_sempervirens',
-  #   'Caladium','Callicarpa_americana','Cercis_canadensis','Calyptocarpus_vialis','Pecan',
-  #   'Chrysactinia_mexicana','Coreopsis_lanceolata','Cotinus_coggygria','Chamaemelum_nobile',
-  #   'Chasmanthium_latifolium','Chasmanthium_sessiliflorum','Chilopsis','Chrysactinia_mexicana',
-  #   'Meyer_lemon','Cordyline_australis','Cordyline_fruticosa','Coreopsis_grandiflora',
-  #   'Echinacea_paradoxa','Euphorbia_myrsinites','Hesperaloe_funifera',
-  #   'Hydrangea_quercifolia','juniperus_horizontalis','lantana_urticoides',
-  #   'Liriope_muscari','magnolia_soulangeana','Miscanthus_sinensis',
-  #   'Muhlenbergia_lindheimeri','Nerium','Opuntia_rufida',
-  #   'Phlox_divaricata','Phoenix_reclinata','Pyrus_calleryana',
-  #   'Rhododendron_japonicum','Rubus_idaeus','Salvia_guaranitica',
-  #   'Salvia_pachyphylla',
-  #   ]
   loadPlantNames()
