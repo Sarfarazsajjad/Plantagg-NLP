f2c83696713ffbaf34f4f47e4989e3ceb26aecb3
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2020-04-16 18:32:49 +0500

    create header first

--- wikipedia_extract_comman_names_by_botanical_names.py
+++ wikipedia_extract_comman_names_by_botanical_names.py
@@ -2,6 +2,11 @@
 import wikipedia as wikipedia
 import csv
 # %%
+# prepare csv header
+with open('wikipedia_pages_by_plant_botanical_names.csv','a') as writeFile:
+            writer = csv.writer(writeFile)
+            writer.writerow(['Botanical Name','Expected Common Name','Wikipedia Page link','Image urls'])
+
 with open('plant_botanical_names.txt','r') as file:
     for line in file:
         print(line)
