0c1856fe7e3b21d91131b51d21816daf5ceb8e94
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2020-04-16 19:56:31 +0500

    created exception clause to handle page not found error

--- wikipedia_extract_comman_names_by_botanical_names.py
+++ wikipedia_extract_comman_names_by_botanical_names.py
@@ -3,19 +3,30 @@ import wikipedia as wikipedia
 import csv
 # %%
 # prepare csv header
-with open('wikipedia_pages_by_plant_botanical_names.csv','a') as writeFile:
-            writer = csv.writer(writeFile)
-            writer.writerow(['Botanical Name','Expected Common Name','Wikipedia Page link','Image urls'])
-
+# with open('wikipedia_pages_by_plant_botanical_names.csv','a') as writeFile:
+#             writer = csv.writer(writeFile)
+#             writer.writerow(['Botanical Name','Expected Common Name','Wikipedia Page link','Image urls'])
+# %%
+# fetch wikipedia data
 with open('plant_botanical_names.txt','r') as file:
     for line in file:
-        print(line)
+        plantName = line.replace("\n", "")
+        print(str(plantName))
 # %%
-        pageObject = wikipedia.page(line)
-        pageHtml = wikipedia.page(line).html()
-        with open('wikipedia_pages_by_plant_botanical_names.csv','a') as writeFile:
-            writer = csv.writer(writeFile)
-            writer.writerow([line.replace("\n", ""),pageObject.title,pageObject.url," ".join(str(x) for x in pageObject.images)])
+        try:
+            pageObject = wikipedia.page(plantName)
+            pageHtml = wikipedia.page(plantName).html()
+            with open('wikipedia_pages_by_plant_botanical_names.csv','a') as writeFile:
+                writer = csv.writer(writeFile)
+                writer.writerow([plantName,pageObject.title,pageObject.url," ".join(str(x) for x in pageObject.images)])
+        except:
+            #if a "PageError" was raised, ignore it and continue to next link
+            with open('wikipedia_pages_by_plant_botanical_names.csv','a') as writeFile:
+                writer = csv.writer(writeFile)
+                writer.writerow([plantName,'not found','not found','not found'])
+            print('##### not found #####')
+            continue
+
 
 
 # %%
