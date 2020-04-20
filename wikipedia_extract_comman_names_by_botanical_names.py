21138f4058400dc19b1bcef0f499469078b3e256
Sarfaraz Hussain <sarfarazsajjad@gmail.com>
2020-04-20 22:12:33 +0500

    extract common names

--- wikipedia_extract_comman_names_by_botanical_names.py
+++ wikipedia_extract_comman_names_by_botanical_names.py
@@ -4,15 +4,15 @@ import csv
 # %%
 # prepare csv header
 with open('wikipedia_pages_by_plant_botanical_names.csv','a') as writeFile:
-            writer = csv.writer(writeFile)
-            writer.writerow(['Botanical Name','Expected Common Name','Wikipedia Page link','Image urls'])
+    writer = csv.writer(writeFile)
+    writer.writerow(['Botanical Name','Expected Common Name','Wikipedia Page link','Image urls'])
 # %%
 # fetch wikipedia data
 with open('plant_botanical_names.txt','r') as file:
     for line in file:
         plantName = line.replace("\n", "")
         print(str(plantName))
-# %%
+        # %%
         try:
             pageObject = wikipedia.page(plantName)
             pageHtml = wikipedia.page(plantName).html()
@@ -26,3 +26,30 @@ with open('plant_botanical_names.txt','r') as file:
                 writer.writerow([plantName,'not found','not found','not found'])
             print('##### not found #####')
             continue
+# %%
+def extract_common_names(soup):
+    all_p_tags = soup.find_all('p')
+    print(len(all_p_tags))
+    b_tags = []
+    for p in all_p_tags:
+        b_tags_in_p = p.find_all('b')
+        for tag in b_tags_in_p:
+            b_tags.append(tag)
+
+    if (len(b_tags)):
+        print(b_tags)
+
+    common_names = []
+    for b_tag in b_tags:
+        common_names.append(b_tag.string)
+
+    print(common_names)
+
+    common_names_iterator = iter(common_names)
+    common_names_string = next(common_names_iterator)
+    for common_name in common_names_iterator:
+        common_names_string = common_names_string + '; ' + common_name
+
+    # print(common_names_string)
+
+    return common_names_string
